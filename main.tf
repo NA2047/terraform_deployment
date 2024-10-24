02ddaa030ea335e7a55a3c1d0ca946ae051fd85b
NA2047 <12290725+NA2047@users.noreply.github.com>
2024-10-24 11:07:35 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "google_filestore_instance" "instance" {
 resource "google_compute_disk" "default11" {
   name  = var.disk_name
   type  = "hyperdisk-throughput"
-  zone  = "us-central1"
+  zone  = "us-central1a"
   provisioned_iops = 49
   size = 5000
    lifecycle {
